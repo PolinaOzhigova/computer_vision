c34cddd64d7909885e8276f8a676fde8320b98b8
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-12-10 00:35:13 +0800

    fix

--- balls_and_rects/main.py
+++ balls_and_rects/main.py
@@ -1,41 +1,36 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from skimage.morphology import binary_erosion
-from skimage.measure import label, regionprops
-from skimage.draw import disk
 import cv2
+import numpy as np
+from skimage.measure import label, regionprops
 
-image = plt.imread('balls_and_rects.png')
-print(image.shape)
+image = cv2.imread('balls_and_rects.png')
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-
-#colors = np.unique(hsv[:, :, 0])
-binary = image.mean(2) > 0
+binary = hsv.mean(2) > 0
 labeled = label(binary)
-# print("max(labeled) ", np.max(labeled))
-
 regions = regionprops(labeled)
-colors = []
-h = hsv[:, :, 0]
+
+circles_colors = {}
+rects_colors = {}
+rects = 0
+circles = 0
 for region in regions:
-    pixels = h[region.coords]
-    r = h[region.bbox[0]:region.bbox[2], region.bbox[1]:region.bbox[3]]
-    colors.extend(np.unique(r)[1:])
-print("number of bulls ", len(colors))
+    color = hsv[region.bbox[0]:region.bbox[2], region.bbox[1]:region.bbox[3], 0]
+    key = np.max(color[0])
+    if np.all(color[0][0] != 0):
+        rects += 1
+        rects_colors[key] = rects_colors.get(key, 0) + 1
+    else:
+        circles += 1
+        circles_colors[key] = circles_colors.get(key, 0) + 1
 
-clusters = []
-while colors:
-    color1 = colors.pop(0)
-    clusters.append([color1])
-    for color2 in colors.copy():
-        if abs(color1 - color2) < 5:
-            clusters[-1].append(color2)
-            colors.pop(colors.index(color2))
+print(f"\nAll figures: {np.max(labeled)}")
+print(f"Rectangles: {rects}")
+print(f"Circles: {circles}\n")
 
-for cluster in clusters:
-    print(len(cluster))
-
-# plt.plot(colors, "0")
-# plt.imshow(h)
-# plt.show()
+print("{:<10} {:<20} {:<20}".format("Shade", "Rects Colors", "Circles Colors"))
+for key in set(rects_colors.keys()).union(circles_colors.keys()):
+    print("{:<10} {:<20} {:<20}".format(
+        key,
+        rects_colors.get(key, ''),
+        circles_colors.get(key, '')
+    ))
