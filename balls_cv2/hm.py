76343efcb76242c9a0350909907e60424a343ad3
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-12-10 00:36:33 +0800

    add code from lesson

--- balls_cv2/hm.py
+++ balls_cv2/hm.py
@@ -3,17 +3,19 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
+capture.set(cv2.CAP_PROP_EXPOSURE, -6)
 cv2.namedWindow("Camera")
 # cv2.namedWindow("Debug")
 
-# lower1 = (90, 150, 120)
-# upper1 = (115, 255, 255) #BLUE
+lower4 = (90, 150, 120)
+upper4 = (115, 255, 255) #BLUE
 
 # lower = (0, 205, 170)
 # upper = (5, 225, 190) #RED BAD
 
-lower2 = (5, 150, 200)
-upper2 = (30, 255, 255) #ORANGE
+lower2 = (0, 150, 200)
+upper2 = (10, 255, 255) #ORANGE
 
 lower1 = (23, 80, 100)
 upper1 = (35, 230, 255) #YELLOW
@@ -35,16 +37,26 @@ def centers(cv2, hsv, lower, upper):
 while capture.isOpened():
     ret, frame = capture.read()
     frame = cv2.GaussianBlur(frame, (21, 21), 0)
-    cv2.putText(frame, f"Show Yellow Orange Green", (10, 30),
+    cv2.putText(frame, f"Show Yellow Orange Green (Blue)", (10, 30),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 127))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     c1 = centers(cv2, hsv, lower1, upper1)
     c2 = centers(cv2, hsv, lower2, upper2)
     c3 = centers(cv2, hsv, lower3, upper3)
-    if True:
-        cv2.putText(frame, f"{c1, c2, c3}", (10, 60),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 127))
+    c4 = centers(cv2, hsv, lower4, upper4)
+    cv2.putText(frame, f"{c1, c2, c3, c4}", (10, 60),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 127))
+    y_max = frame.shape[0] #480
+    x_max = frame.shape[1] #640
+    if(c1 != None and c2 != None and c3 != None):
+        if(c1[0] < c2[0] and c1[1] < c2[1] and c2[0] < c3[0] and c2[1] < c3[1]):
+            cv2.putText(frame, f"DONE!", (10, 90),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 127))
+        if(c4 != None):
+            if(c1[1] < c2[1] and c3[1] < c4[1]):
+                cv2.putText(frame, f"DONE2!", (10, 90),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 127))
 
     # cv2.imshow("Debug", mask)
     cv2.imshow("Camera", frame)
