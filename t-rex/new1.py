68fbb637d317069faa9768e6d6869dfe2c77c817
polina <117908658+PolinaOzhigova@users.noreply.github.com>
2023-12-13 02:04:50 +0800

    dino find 820

--- t-rex/new1.py
+++ t-rex/new1.py
@@ -1,29 +1,86 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 import mss
 import pyautogui
+import time
+
+# time_sleep = 0.18
+
+def find_c():
+    try:
+        image_location = pyautogui.locateOnScreen('c.png')
+        return True
+    except pyautogui.ImageNotFoundException:
+        return False
+
+def find_dino():
+    image_location = None
+    while image_location is None:
+        try:
+            image_location = pyautogui.locateOnScreen('dino.png')
+            print(f"Изображение найдено. Координаты: {image_location}")
+            pyautogui.press("space")
+        except pyautogui.ImageNotFoundException:
+            print("Изображение не найдено.")
+    d_left, d_top, d_width, d_height = image_location
+    point_check = (d_left + d_width + 50, d_top)
+    return point_check, d_left, d_width
+
+point_check, d_left, d_width = find_dino()
+# time_sleep = 0.1
+# print(point_check)
+# f = False
+
+i = 150
 
 with mss.mss() as sct:
-    monitor = {"top": 270, "left": 480, "width": 920, "height": 250}
+    last_space_press_time = time.time()
+    # count = 0
 
     while "Screen capturing":
-        img = np.array(sct.grab(monitor))
-        # img_np = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        # gray_img = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
-        # _, binary_img = cv2.threshold(gray_img, 127, 255, cv2.THRESH_BINARY)
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        binary_img = hsv.mean(2) > 0
+        current_time = time.time()
+        f = False
+        # print(current_time - last_space_press_time)
+        if current_time - last_space_press_time >= 20 and f == False:
+            f = True
+            last_space_press_time = current_time
+        if current_time - last_space_press_time >= 5 and f == True:
+            # time_sleep -= 0.005
+            i += 1
+            # print(i)
+            # count += current_time - last_space_press_time
+            last_space_press_time = current_time
+        # if current_time - last_space_press_time >= 2 and count >=50:
+        #     time_sleep -= 0.008
+        #     last_space_press_time = current_time
+        #     i += 5
 
-        point_check = (500, 190)
-        square_size = 30
-        top_left = (point_check[0] - square_size // 2, point_check[1] - square_size // 2)
-        bottom_right = (point_check[0] + square_size // 2, point_check[1] + square_size // 2)
+        img = np.array(sct.grab(sct.monitors[1]))
+        
+        img = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+        _, binary_img = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+        
+        # binary_img[point_check[1]:point_check[1]+5, point_check[0]:point_check[0]+i] = 170
+        # plt.imshow(binary_img)
+        # plt.show()
 
-        region_of_interest = (binary_img * 255).astype(np.uint8)
+        if(find_c()):
+            i -= 20
+        if np.any(binary_img[point_check[1]:point_check[1]+30, point_check[0]:point_check[0]+i] == 0):
+            pyautogui.press("space")
+            time.sleep(0.001)
+            # if (time_sleep > 0):
+            #     time.sleep(time_sleep)
+            # if not np.all(binary_img[point_check[1]+20:point_check[1]+25, d_left:d_left+d_width+150] == 1):
+            #     pyautogui.press('down')
+        if np.any(binary_img[point_check[1]:point_check[1]+30, point_check[0]:point_check[0]+i-10] == 0) and np.all(binary_img[point_check[1]+25:point_check[1]+40, d_left:d_left+d_width+i-120] == 255):
+            pyautogui.PAUSE = 0
+            pyautogui.keyDown('down')
+            time.sleep(0.002)
+            pyautogui.keyUp('down')
+            # pyautogui.press('down')
+            pyautogui.PAUSE = 0.1
+            print("down")
 
-        cv2.imshow("Region of Interest", region_of_interest)
-    
-        # Press "q" to quit
-        if cv2.waitKey(25) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            break
+cv2.destroyAllWindows()
